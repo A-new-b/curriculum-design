49230e138d1aa95cc283dcc1643e73cc52291c90
Boyu <714967142@qq.com>
2020-04-19 13:18:23 +0800

    delete

--- experiment1_BoyuGuan/homework_datastruct/register.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "supermarket.h"
-
-//上新物品检查
-
-void new_Item(big_category &catalog, ElemType e);
-
-void register_category(big_category &category, const char a[]);
--- experiment1_BoyuGuan/homework_datastruct/search.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "supermarket.h"
-
-LinkList search_Item( big_category category , const char name[]);
-
-big_category search_Big_Category(big_category category, char name[]);
-
-int search_Items_Sold_Out(big_category category);
-
